d98f231e856cfd642eeef61b402643067bcdcd7c
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2017-09-05 12:52:48 -0300

    beautify the output with colors and add more meta info

--- gateway.sh
+++ gateway.sh
@@ -12,15 +12,19 @@ last_entry_name() {
     tail -n 1 ~/.scheduler_history | cut  -d '|' -f2 | tr -d "\n"
 }
 
+last_studied_date_for_subject() {
+    tac ~/.scheduler_history | grep $1 | head -n1 | cut -d '|' -f1 | tr -d "\n"
+}
+
 days_since_last_study() {
-    date=$(tac ~/.scheduler_history | grep $1 | head -n1 | cut -d '|' -f1)
+    date=$(last_studied_date_for_subject $1)
 
     # subject never studied
     [ -z "$date" ] && {
         echo 15
         return
     }
-
+    days=$(echo $(( ( $(date -ud "$date" +'%s') - $(date +'%s') )/60/60/24 )))
     days=$(date_diff.js "$date" "$(date)" | jq .days)
 
     [[ $days ==  'null' ]] && {
--- scheduler.py
+++ scheduler.py
@@ -5,9 +5,10 @@ import subprocess
 import datetime
 
 def gateway(params):
-    return subprocess.run(params, stdout=subprocess.PIPE).stdout.decode('UTF-8')
+    prefix = ['./gateway.sh']
+    return subprocess.run(prefix + params, stdout=subprocess.PIPE).stdout.decode('UTF-8')
 
-subjects = gateway(['./gateway.sh', 'list_subjects_names'])
+subjects = gateway(['list_subjects_names'])
 
 
 #build the initial dic
@@ -17,38 +18,41 @@ for entry in subjects.splitlines() :
 
 # change values based on the importance of the subject configured
 for subject in subjects_weights:
-    importance_str = gateway(['./gateway.sh', 'get_weight_by_name', subject])
+    importance_str = gateway(['get_weight_by_name', subject])
     importance = int(importance_str)
-    subjects_weights[subject]+= subjects_weights[subject] * (importance * 0.1)
+    subjects_weights[subject]+= subjects_weights[subject] * (importance * 0.5)
 
 # give less probability to the latest and more to the earlier
 for subject in subjects_weights:
-    days_since_last_study =  int(gateway(['./gateway.sh', 'days_since_last_study', subject]))
+    days_since_last_study =  int(gateway(['days_since_last_study', subject]))
     subjects_weights[subject]+=  subjects_weights[subject] * days_since_last_study
 
 
 # turns the last one less probable to repeat
-last_entry =  gateway(['./gateway.sh', 'last_entry_name'])
+last_entry =  gateway(['last_entry_name'])
 subjects_weights[last_entry] = subjects_weights[last_entry] / 4
 
 # give more probability to new subjects (which were never used)
-new_subjects =  gateway(['./gateway.sh', 'new_subjects'])
+new_subjects =  gateway(['new_subjects'])
 for subject in new_subjects.splitlines() :
-    subjects_weights[subject] =  subjects_weights[subject]  * 2
+    subjects_weights[subject] =  subjects_weights[subject] * 2
+
+# -- contextual calculai in the end --
 
 # calculate the energy levels
 now = datetime.datetime.now()
 #low energy level period
 if now.hour > 22 or now.hour < 4:
     for subject in subjects_weights:
-        energy_level = int(gateway(['./gateway.sh', 'get_energy_level_by_name', subject]))
+        energy_level = int(gateway(['get_energy_level_by_name', subject]))
         subjects_weights[subject] = subjects_weights[subject] * (1/energy_level)
+# -- printing ---
 
 def print_result(subjects_weights):
     sorted_subjects  = sorted(subjects_weights.items(), key=operator.itemgetter(1), reverse=True)
     for subject,weight in sorted_subjects:
-        what_todo = gateway(['./gateway.sh', 'get_whattodo_details_by_name', subject])
-        print (subject + ": " + what_todo)
+        what_todo = gateway(['get_whattodo_details_by_name', subject])
+        last_date_studied = gateway(['last_studied_date_for_subject', subject])
+        print ('\x1b[6;30;42m' + subject + '\x1b[0m' + '|' + what_todo + '|' + last_date_studied)
 
 print_result(subjects_weights)
-
