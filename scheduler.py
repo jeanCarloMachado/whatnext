beadfdfc6233538dc927534fe6a1465a4d3fdb07
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2018-01-15 20:33:54 -0200

    improve wn listing

--- scheduler.py
+++ scheduler.py
@@ -6,6 +6,8 @@ import datetime
 import sys
 import math
 import os
+from timePerSubject import time_of_subjects
+from timeToStr import minutes_to_str
 
 def memoize(function):
     from functools import wraps
@@ -26,16 +28,19 @@ def gateway(params):
     return subprocess.run(prefix + params, stdout=subprocess.PIPE).stdout.decode('UTF-8')
 
 class Subject:
-    def __init__(self, name, weight, priority, complexity, whatToDoNext, daysSinceLastStudy):
+    def __init__(self, name, weight, priority, complexity, what_to_do_next, days_since_last_study, time_already_invested):
         self.name = name
         self.weight = weight
         self.priority = priority
         self.complexity = complexity
-        self.whatToDoNext = whatToDoNext
-        self.daysSinceLastStudy = daysSinceLastStudy
+        self.what_to_do_next = what_to_do_next
+        self.days_since_last_study = days_since_last_study
+        self.time_already_invested = time_already_invested
+
 
 def factory_subjects():
     subjects = gateway(['listSubjects'])
+    time_already_invested = time_of_subjects()
     #build the initial dic
     subjects_configs = {}
     for line in subjects.splitlines() :
@@ -45,14 +50,13 @@ def factory_subjects():
                 weight=1,
                 priority=int(columns[1]),
                 complexity=int(columns[2]),
-                whatToDoNext=columns[3],
-                daysSinceLastStudy=gateway(['daysSinceLastStudy', columns[0]])
+                what_to_do_next=columns[3],
+                days_since_last_study=gateway(['daysSinceLastStudy', columns[0]]),
+                time_already_invested=time_already_invested[columns[0]] if columns[0] in time_already_invested  else 0
         )
 
     return subjects_configs
 
-
-subjects_configs = factory_subjects()
 
 # change values based on the importance of the subject configured
 def configure_importance(subjects_configs):
@@ -61,34 +65,39 @@ def configure_importance(subjects_configs):
 
     return subjects_configs
 
-subjects_configs = configure_importance(subjects_configs)
+def configure_subjects():
+    subjects_configs = factory_subjects()
+    subjects_configs = configure_importance(subjects_configs)
 
-# give less probability to the latest and more to the earlier
-for subject in subjects_configs:
-    if subjects_configs[subject].daysSinceLastStudy == "":
-        subjects_configs[subject].weight = (subjects_configs[subject].weight  * subjects_configs[subject].weight * 0.2)
-        continue
-    daysSinceLastStudy =  int(subjects_configs[subject].daysSinceLastStudy)
-    subjects_configs[subject].weight += subjects_configs[subject].weight * daysSinceLastStudy
-
-# turns the last one less probable to repeat
-last_entry =  gateway(['lastEntryName'])
-if last_entry in subjects_configs:
-    subjects_configs[last_entry].weight = subjects_configs[last_entry].weight / 4
-
-# give more probability to new subjects (which were never used)
-new_subjects =  gateway(['new_subjects'])
-for subject in new_subjects.splitlines() :
-    subjects_configs[subject].weight =  subjects_configs[subject].weight * 1.1
-
-# -- contextual calculai in the end --
-
-now = datetime.datetime.now()
-#tired period
-if (now.hour > 22 or now.hour < 4) or os.environ.get('TIRED') is not None:
+    # give less probability to the latest and more to the earlier
     for subject in subjects_configs:
-        base = (1 / math.pow(subjects_configs[subject].complexity,9))
-        subjects_configs[subject].weight = subjects_configs[subject].weight *  base 
+        if subjects_configs[subject].days_since_last_study == "":
+            subjects_configs[subject].weight = (subjects_configs[subject].weight  * subjects_configs[subject].weight * 0.2)
+            continue
+        days_since_last_study =  int(subjects_configs[subject].days_since_last_study)
+        subjects_configs[subject].weight += subjects_configs[subject].weight * days_since_last_study
+
+    # turns the last one less probable to repeat
+    last_entry =  gateway(['lastEntryName'])
+    if last_entry in subjects_configs:
+        subjects_configs[last_entry].weight = subjects_configs[last_entry].weight / 4
+
+    # give more probability to new subjects (which were never used)
+    new_subjects =  gateway(['new_subjects'])
+    for subject in new_subjects.splitlines() :
+        subjects_configs[subject].weight =  subjects_configs[subject].weight * 1.1
+
+    # -- contextual calculai in the end --
+
+    now = datetime.datetime.now()
+    #tired period
+    if (now.hour > 22 or now.hour < 4) or os.environ.get('TIRED') is not None:
+        for subject in subjects_configs:
+            base = (1 / math.pow(subjects_configs[subject].complexity,9))
+            subjects_configs[subject].weight = subjects_configs[subject].weight *  base 
+
+    return subjects_configs
+
 # -- printing ---
 
 orange = os.getenv('WN_COLOR_ORANGE').encode('utf-8').decode('unicode_escape')
@@ -97,25 +106,34 @@ red = os.getenv('WN_COLOR_RED').encode('utf-8').decode('unicode_escape')
 reset = os.getenv('WN_COLOR_RESET').encode('utf-8').decode('unicode_escape')
 title = os.getenv('WN_COLOR_TITLE').encode('utf-8').decode('unicode_escape')
 
+
+def get_days_since_last_study_str(subject):
+    days_since_last_study_str = subject.days_since_last_study
+    days_since_last_study_int = int(days_since_last_study_str) if days_since_last_study_str.isdigit() else 0
+
+    if days_since_last_study_int < 7:
+        daysColor = green
+    elif days_since_last_study_int > 7 and days_since_last_study_int < 20:
+        daysColor = orange
+    else:
+        daysColor = red
+
+    if days_since_last_study_str == '':
+        days_since_last_study_str = ''
+        daysColor = orange
+    else:
+        days_since_last_study_str =  daysColor  + days_since_last_study_str + ' days ago' 
+
+    return days_since_last_study_str
+
 def print_result(subjects_configs):
     sorted_subjects  = sorted(subjects_configs.items(), key=lambda x: x[1].weight, reverse=True)
     for subject,weight in sorted_subjects:
-        days_since_last_study_str = subjects_configs[subject].daysSinceLastStudy
-        days_since_last_study_int = int(days_since_last_study_str) if days_since_last_study_str.isdigit() else 0
+        days_since_last_study_str = get_days_since_last_study_str(subjects_configs[subject])
+        time_invested = minutes_to_str(subjects_configs[subject].time_already_invested)
+        time_invested = " - " + time_invested  if len(time_invested) > 4 else "" 
 
-        if days_since_last_study_int < 7:
-            daysColor = green
-        elif days_since_last_study_int > 7 and days_since_last_study_int < 20:
-            daysColor = orange
-        else:
-            daysColor = red
 
-        if days_since_last_study_str == '':
-            days_since_last_study_str = 'never '
-            daysColor = orange
-        else:
-            days_since_last_study_str+=  ' days ago '
+        print (title + ' ' + subject + reset + ' (' + days_since_last_study_str + '' + time_invested + reset + ') ' + subjects_configs[subject].what_to_do_next + reset)
 
-        print (title + ' ' + subject + reset + daysColor + ' ' + days_since_last_study_str + reset + '' + subjects_configs[subject].whatToDoNext + reset)
-
-print_result(subjects_configs)
+print_result(configure_subjects())
--- timePerSubject.py
+++ timePerSubject.py
@@ -9,76 +9,85 @@ from datetime import datetime, timedelta
 import os
 from timeToStr import minutes_to_str
 
-if len(sys.argv) > 1 and any(map(lambda x: x == "help", sys.argv)):
-    print("""Prints the total of time one spent on each subject
-Usage:
-
-    program month               returns the totals of the month
-    program week                returns the totals of the year
-
-            """)
-    sys.exit()
-
-
 humanMode=False
-if len(sys.argv) > 1 and any(map(lambda x: x == "human", sys.argv)):
-    humanMode=True
 
 def gateway(params):
     prefix = [ os.path.dirname(os.path.realpath(__file__)) + '/gateway.sh']
     return subprocess.run(prefix + params, stdout=subprocess.PIPE).stdout.decode('UTF-8')
 
+def time_of_subjects(dateStart=None, dateEnd=None):
+    if dateStart is None:
+        dateStart = datetime.strptime('1970-01-01', '%Y-%m-%d')
+    if dateEnd is None:
+        dateEnd = datetime.strptime('3000-01-01', '%Y-%m-%d')
+
+    history = gateway(['listHistory'])
+    subjectsConfigs = {}
+    subjects = gateway(['listHistory'])
+    #build the initial dic
+    subjectData = {}
+
+    for line in subjects.splitlines():
+        columns  = line.split('|')
+        name=columns[1]
+        doneStartStr=columns[0]
+        doneStart = datetime.strptime(doneStartStr, '%Y-%m-%d %H:%M:%S')
+
+        if doneStart > dateStart and doneStart < dateEnd:
+            subjectData[name] = subjectData[name]+50 if name in subjectData else 50;
+
+    return subjectData
 
 
-dateStart = datetime.strptime('1970-01-01', '%Y-%m-%d')
-dateEnd = datetime.strptime('3000-01-01', '%Y-%m-%d')
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and any(map(lambda x: x == "help", sys.argv)):
+        print("""The total of time one spent on each subject
+    Usage:
 
-if len(sys.argv) > 1 and any(map(lambda x: x == "month", sys.argv)):
-    dateStart = datetime.now().replace(day=1)
-    dateEnd = dateStart + timedelta(days=30) 
-if len(sys.argv) > 1 and any(map(lambda x: x == "week", sys.argv)):
-    today = datetime.now()
-    dateStart = today - timedelta(days=today.weekday())
-    dateEnd = dateStart + timedelta(days=6)
+        program month               returns the totals of the month
+        program week                returns the totals of the year
 
-if len(list(filter(lambda x: x != "--no-color", sys.argv))) == 3:
-    dateStart = datetime.strptime(sys.argv[1], '%Y-%m-%d')
-    dateEnd = datetime.strptime(sys.argv[2], '%Y-%m-%d')
-
-resetColor = os.getenv('WN_COLOR_RESET').encode('utf-8').decode('unicode_escape')
-titleColor = os.getenv('WN_COLOR_TITLE').encode('utf-8').decode('unicode_escape')
-orange = os.getenv('WN_COLOR_ORANGE').encode('utf-8').decode('unicode_escape')
+                """)
+        sys.exit()
 
 
-if os.environ.get('NO_COLOR') is not None:
-    titleColor=''
-    resetColor=''
-    orange=''
+    if len(sys.argv) > 1 and any(map(lambda x: x == "human", sys.argv)):
+        humanMode=True
 
-history = gateway(['listHistory'])
-subjectsConfigs = {}
-subjects = gateway(['listHistory'])
-#build the initial dic
-subjectData = {}
+    dateStart = None
+    dateEnd = None
+    if len(sys.argv) > 1 and any(map(lambda x: x == "month", sys.argv)):
+        dateStart = datetime.now().replace(day=1)
+        dateEnd = dateStart + timedelta(days=30) 
+    if len(sys.argv) > 1 and any(map(lambda x: x == "week", sys.argv)):
+        today = datetime.now()
+        dateStart = today - timedelta(days=today.weekday())
+        dateEnd = dateStart + timedelta(days=6)
 
-for line in subjects.splitlines():
-    columns  = line.split('|')
-    name=columns[1]
-    doneStartStr=columns[0]
-    doneStart = datetime.strptime(doneStartStr, '%Y-%m-%d %H:%M:%S')
+    if len(list(filter(lambda x: x != "--no-color", sys.argv))) == 3:
+        dateStart = datetime.strptime(sys.argv[1], '%Y-%m-%d')
+        dateEnd = datetime.strptime(sys.argv[2], '%Y-%m-%d')
 
-    if doneStart > dateStart and doneStart < dateEnd:
-        subjectData[name] = subjectData[name]+50 if name in subjectData else 50;
+    subjectData=time_of_subjects(dateStart, dateEnd)
 
-sortedSubjects = sorted(subjectData.items(), key=lambda value: value[1], reverse=True)
+    sortedSubjects = sorted(subjectData.items(), key=lambda value: value[1], reverse=True)
+
+    resetColor = os.getenv('WN_COLOR_RESET').encode('utf-8').decode('unicode_escape')
+    titleColor = os.getenv('WN_COLOR_TITLE').encode('utf-8').decode('unicode_escape')
+    orange = os.getenv('WN_COLOR_ORANGE').encode('utf-8').decode('unicode_escape')
 
 
-total = 0
-for item in sortedSubjects:
-    total+= item[1]
-    timeInSubject = minutes_to_str(item[1]) if (humanMode) else  str(item[1])
-    print( titleColor + item[0] + resetColor + ": " + orange + timeInSubject + resetColor)
+    if os.environ.get('NO_COLOR') is not None:
+        titleColor=''
+        resetColor=''
+        orange=''
 
-total = minutes_to_str(total) if (humanMode) else  str(total)
+    total = 0
+    for item in sortedSubjects:
+        total+= item[1]
+        timeInSubject = minutes_to_str(item[1]) if (humanMode) else  str(item[1])
+        print( titleColor + item[0] + resetColor + ": " + orange + timeInSubject + resetColor)
 
-print( titleColor + "Total" + resetColor + ": " + orange + total + resetColor)
+    total = minutes_to_str(total) if (humanMode) else  str(total)
+
+    print( titleColor + "Total" + resetColor + ": " + orange + total + resetColor)
