4ae5ec4eb5e9df5271c8ec4866c769c07c7c2964
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2018-01-20 14:17:05 -0200

    enable consume wn list via web

--- src/scheduler.py
+++ src/scheduler.py
@@ -11,17 +11,6 @@ from timeToStr import minutes_to_str
 from gateway import gateway
 
 
-class Subject:
-    def __init__(self, name, weight, priority, complexity, what_to_do_next, days_since_last_study, time_already_invested):
-        self.name = name
-        self.weight = weight
-        self.priority = priority
-        self.complexity = complexity
-        self.what_to_do_next = what_to_do_next
-        self.days_since_last_study = days_since_last_study
-        self.time_already_invested = time_already_invested
-
-
 def factory_subjects():
     subjects = gateway(['listSubjects'])
     time_already_invested = time_of_subjects()
@@ -29,15 +18,15 @@ def factory_subjects():
     subjects_configs = {}
     for line in subjects.splitlines() :
         columns  = line.split('|')
-        subjects_configs[columns[0]] = Subject(
-                name=columns[0],
-                weight=1,
-                priority=int(columns[1]),
-                complexity=int(columns[2]),
-                what_to_do_next=columns[3],
-                days_since_last_study=gateway(['daysSinceLastStudy', columns[0]]),
-                time_already_invested=time_already_invested[columns[0]] if columns[0] in time_already_invested  else 0
-        )
+        subjects_configs[columns[0]] = {
+                'name': columns[0],
+                'weight': 1,
+                'priority': int(columns[1]),
+                'complexity': int(columns[2]),
+                'what_to_do_next': columns[3],
+                'days_since_last_study': gateway(['daysSinceLastStudy', columns[0]]),
+                'time_already_invested': time_already_invested[columns[0]] if columns[0] in time_already_invested  else 0
+        }
 
     return subjects_configs
 
@@ -45,7 +34,7 @@ def factory_subjects():
 # change values based on the importance of the subject configured
 def configure_importance(subjects_configs):
     for subject in subjects_configs:
-        subjects_configs[subject].weight += subjects_configs[subject].weight * (math.pow(subjects_configs[subject].priority, 2) * 0.9)
+        subjects_configs[subject]['weight'] += subjects_configs[subject]['weight'] * (math.pow(subjects_configs[subject]['priority'], 2) * 0.9)
 
     return subjects_configs
 
@@ -55,21 +44,21 @@ def configure_subjects():
 
     # give less probability to the latest and more to the earlier
     for subject in subjects_configs:
-        if subjects_configs[subject].days_since_last_study == "":
-            subjects_configs[subject].weight = (subjects_configs[subject].weight  * subjects_configs[subject].weight * 0.2)
+        if subjects_configs[subject]['days_since_last_study'] == "":
+            subjects_configs[subject]['weight'] = (subjects_configs[subject]['weight']  * subjects_configs[subject]['weight'] * 0.2)
             continue
-        days_since_last_study =  int(subjects_configs[subject].days_since_last_study)
-        subjects_configs[subject].weight += subjects_configs[subject].weight * days_since_last_study
+        days_since_last_study =  int(subjects_configs[subject]['days_since_last_study'])
+        subjects_configs[subject]['weight'] += subjects_configs[subject]['weight'] * days_since_last_study
 
     # turns the last one less probable to repeat
     last_entry =  gateway(['lastEntryName'])
     if last_entry in subjects_configs:
-        subjects_configs[last_entry].weight = subjects_configs[last_entry].weight / 4
+        subjects_configs[last_entry]['weight'] = subjects_configs[last_entry]['weight'] / 4
 
     # give more probability to new subjects (which were never used)
     new_subjects =  gateway(['new_subjects'])
     for subject in new_subjects.splitlines() :
-        subjects_configs[subject].weight =  subjects_configs[subject].weight * 1.1
+        subjects_configs[subject]['weight'] =  subjects_configs[subject]['weight'] * 1.1
 
     # -- contextual calculai in the end --
 
@@ -78,57 +67,61 @@ def configure_subjects():
     if (now.hour > 22 or now.hour < 4) or os.environ.get('TIRED') is not None:
         for subject in subjects_configs:
             base = (1 / math.pow(subjects_configs[subject].complexity,9))
-            subjects_configs[subject].weight = subjects_configs[subject].weight *  base 
+            subjects_configs[subject]['weight'] = subjects_configs[subject]['weight'] *  base 
 
     return subjects_configs
 
+def sort_subjects(configured_subjects):
+    return  sorted(configured_subjects.items(), key=lambda x: x[1]['weight'], reverse=True)
+
+
 # -- printing ---
+if __name__ == '__main__':
+    orange = os.getenv('WN_COLOR_ORANGE').encode('utf-8').decode('unicode_escape')
+    green = os.getenv('WN_COLOR_GREEN').encode('utf-8').decode('unicode_escape')
+    red = os.getenv('WN_COLOR_RED').encode('utf-8').decode('unicode_escape')
+    reset = os.getenv('WN_COLOR_RESET').encode('utf-8').decode('unicode_escape')
+    title = os.getenv('WN_COLOR_TITLE').encode('utf-8').decode('unicode_escape')
+    section_color = os.getenv('WN_COLOR_SECTION').encode('utf-8').decode('unicode_escape')
 
-orange = os.getenv('WN_COLOR_ORANGE').encode('utf-8').decode('unicode_escape')
-green = os.getenv('WN_COLOR_GREEN').encode('utf-8').decode('unicode_escape')
-red = os.getenv('WN_COLOR_RED').encode('utf-8').decode('unicode_escape')
-reset = os.getenv('WN_COLOR_RESET').encode('utf-8').decode('unicode_escape')
-title = os.getenv('WN_COLOR_TITLE').encode('utf-8').decode('unicode_escape')
-section_color = os.getenv('WN_COLOR_SECTION').encode('utf-8').decode('unicode_escape')
-
-if os.environ.get('NO_COLOR') is not None:
-    orange=''
-    green=''
-    red=''
-    reset=''
-    title=''
+    if os.environ.get('NO_COLOR') is not None:
+        orange=''
+        green=''
+        red=''
+        reset=''
+        title=''
 
 
-def get_days_since_last_study_str(subject):
-    days_since_last_study_str = subject.days_since_last_study
-    days_since_last_study_int = int(days_since_last_study_str) if days_since_last_study_str.isdigit() else 0
+    def get_days_since_last_study_str(subject):
+        days_since_last_study_str = subject['days_since_last_study']
+        days_since_last_study_int = int(days_since_last_study_str) if days_since_last_study_str.isdigit() else 0
 
-    if days_since_last_study_int < 7:
-        daysColor = green
-    elif days_since_last_study_int > 7 and days_since_last_study_int < 30:
-        daysColor = orange
-    else:
-        daysColor = red
+        if days_since_last_study_int < 7:
+            daysColor = green
+        elif days_since_last_study_int > 7 and days_since_last_study_int < 30:
+            daysColor = orange
+        else:
+            daysColor = red
 
-    if days_since_last_study_str == '':
-        days_since_last_study_str = ''
-        daysColor = orange
-    else:
-        days_since_last_study_str =  daysColor  + days_since_last_study_str + ' days ago' 
+        if days_since_last_study_str == '':
+            days_since_last_study_str = ''
+            daysColor = orange
+        else:
+            days_since_last_study_str =  daysColor  + days_since_last_study_str + ' days ago' 
 
-    return days_since_last_study_str
+        return days_since_last_study_str
 
-def print_result(subjects_configs):
-    sorted_subjects  = sorted(subjects_configs.items(), key=lambda x: x[1].weight, reverse=True)
-    counter = 1
-    first = True
-    for subject,weight in sorted_subjects:
-        days_since_last_study_str = get_days_since_last_study_str(subjects_configs[subject])
-        time_invested = green + minutes_to_str(subjects_configs[subject].time_already_invested) +  reset
+    def print_result(subjects_configs):
+        sorted_subjects = sort_subjects(subjects_configs)
+        counter = 1
+        first = True
+        for subject,weight in sorted_subjects:
+            days_since_last_study_str = get_days_since_last_study_str(subjects_configs[subject])
+            time_invested = green + minutes_to_str(subjects_configs[subject]['time_already_invested']) +  reset
 
-        time_invested = " - " + time_invested  if len(time_invested) > 4 else "" 
+            time_invested = " - " + time_invested  if len(time_invested) > 4 else "" 
 
-        print ( title + str(counter) + '. ' + subject + reset + ': ' + days_since_last_study_str + reset + time_invested + reset + ' ' + subjects_configs[subject].what_to_do_next + reset)
-        counter += 1
+            print ( title + str(counter) + '. ' + subject + reset + ': ' + days_since_last_study_str + reset + time_invested + reset + ' ' + subjects_configs[subject]['what_to_do_next'] + reset)
+            counter += 1
 
-print_result(configure_subjects())
+    print_result(configure_subjects())
--- src/webserver.py
+++ src/webserver.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python
+
+
+from scheduler import configure_subjects, sort_subjects
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+app.debug=True
+
+@app.route('/')
+def index():
+    subjects = configure_subjects()
+    sorted_subjects = sort_subjects(subjects)
+    return json.dumps(sorted_subjects)
+
+app.run(host= '0.0.0.0')
