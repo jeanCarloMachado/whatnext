1707fc05a17d1ff21e77648ca4e115caf2c15422
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2018-01-22 19:34:46 -0200

    list working

--- src/web/index.elm
+++ src/web/index.elm
@@ -1,3 +1,5 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
@@ -6,67 +8,106 @@ import Platform exposing (..)
 import Json.Decode exposing (..)
 import Json.Decode.Pipeline exposing (..)
 
+
 main =
     Html.program { init = init, view = view, update = update, subscriptions = subscriptions }
 
+
+
 --Model
+
+
 type alias Subject =
-    {
-        name: String
+    { name : String
     }
 
 
+subjectList =
+    (Subject "initial")
+
+
+
 -- Init
-init : (Subject, Cmd Msg)
+
+
+init : ( List Subject, Cmd Msg )
 init =
-    (Subject "now i become death",  getList)
+    ( [ Subject "now i become death" ], getList )
+
 
 
 -- UPDATE
-type Msg
- = Empty | NewList (Result Http.Error Subject)
 
-update : Msg -> Subject -> (Subject, Cmd Msg)
+
+type Msg
+    = NewList (Result Http.Error (List Subject))
+
+
+update : Msg -> List Subject -> ( List Subject, Cmd Msg )
 update msg model =
     case msg of
         NewList (Err msg) ->
-            ( {  model | name = toString msg}, Cmd.none)
-        NewList (Ok newModel) ->
-            (newModel, Cmd.none)
-        _ ->
-            (model, Cmd.none)
+            ( [], Cmd.none )
 
-getList :  Cmd Msg
+        NewList (Ok subjects) ->
+            ( subjects, Cmd.none )
+
+
+getList : Cmd Msg
 getList =
     let
         url =
-          "http://whatnext:5000"
+            "http://whatnext:5000"
+
         request =
-            Http.get url decodeSubjects
+            Http.get url decodeAll
     in
         Http.send NewList request
 
-decodeSubjects : Decoder Subject
-decodeSubjects =
-    at [ "0" ] decodeSubject
+
+decodeAll : Decoder (List Subject)
+decodeAll =
+    Json.Decode.list decodeSubject
 
 
 decodeSubject : Decoder Subject
 decodeSubject =
-        Json.Decode.Pipeline.decode Subject
-        |>  Json.Decode.Pipeline.required "name" (Json.Decode.string)
+    Json.Decode.Pipeline.decode Subject
+        |> Json.Decode.Pipeline.required "name" (Json.Decode.string)
+
+
 
 -- VIEW
-view : Subject -> Html Msg
-view model =
-    div []
-    [
-        ul []
-            [
-                li [] [text model.name]
+
+
+subjectToHtml : Subject -> Html Msg
+subjectToHtml subject =
+    li []
+        [ div []
+            [ text subject.name
             ]
-    ]
+        ]
+
+
+subjectsToHtml list =
+    let
+        innerList =
+            List.map subjectToHtml list
+    in
+        ul [] innerList
+
+
+view : List Subject -> Html Msg
+view list =
+    div []
+        [ subjectsToHtml list
+        ]
+
+
 
 -- SUBSCRIPTIONS
-subscriptions : Subject -> Sub Msg
-subscriptions model = Sub.none
+
+
+subscriptions : List Subject -> Sub Msg
+subscriptions model =
+    Sub.none
