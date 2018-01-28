b67618b2a30e3f6bdae145ed3833aa19fff88f14
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2018-01-28 18:16:04 -0200

    improvements

--- src/gateway.py
+++ src/gateway.py
@@ -1,19 +1,6 @@
 import os
 import subprocess
 
-def memoize(function):
-    from functools import wraps
-    memo = {}
-    @wraps(function)
-    def wrapper(*args):
-        if str(args) in memo:
-            return memo[str(args)]
-        else:
-            rv = function(*args)
-            memo[str(args)] = rv
-            return rv
-    return wrapper
-
 def gateway(params):
     prefix = [ os.path.dirname(os.path.realpath(__file__)) + '/gateway.sh']
     return subprocess.run(prefix + params, stdout=subprocess.PIPE).stdout.decode('UTF-8')
