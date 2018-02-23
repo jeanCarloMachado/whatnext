6b61c59648e6ce16cda326eb12cb64bb0f1d09a1
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2018-02-23 16:04:51 -0300

    fix signup

--- src/init.sh
+++ src/init.sh
@@ -4,13 +4,13 @@ echo $WHATNEXT_CONF
 mkdir -p $(dirname "$WHATNEXT_CONF")
 
 [ ! -f "$WHATNEXT_CONF" ] && {
-   echo "my first subject|50|50|do something" > "$WHATNEXT_CONF"
+   echo "my first subject|50|50|do something|" > "$WHATNEXT_CONF"
 }
 
 [ ! -f "$WHATNEXT_HISTORY" ] && {
    touch "$WHATNEXT_HISTORY"
    signupDate=$(date "+%Y-%m-%d %H:%M:%S")
-   echo "$signupDate|studies|started using whatnext!||" > "$WHATNEXT_HISTORY"
+   echo "$signupDate|studies|started using whatnext!|" > "$WHATNEXT_HISTORY"
 
 }
 
