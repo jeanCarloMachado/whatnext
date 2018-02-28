1bb6aa4c227799457e7263e77895f6ecbda47a52
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2018-02-28 07:56:44 -0300

    improvements

--- src/Scheduler.hs
+++ src/Scheduler.hs
@@ -6,21 +6,25 @@ import System.Process
 import Data.Aeson
 import Data.ByteString.Lazy.Char8
 import GHC.Generics
+import Data.List
 
 
 
 main = do
-        content <- readProcess "./conf2json.sh" ["chemistry"] ""
+        content <- readProcess "./conf2json.sh" [] ""
         let byteVersion = pack content
-        d <- (decode <$>  (return byteVersion))
+        d <- (decode <$>  (return byteVersion)) :: IO (Maybe [Subject])
         case d of
-          Nothing -> Prelude.putStrLn "adsfad"
-          Just ps -> Prelude.putStrLn $ "The result is: " ++ (name ps)
+          Just ps -> Prelude.putStrLn $ "The result is: " ++ "aa"
+          _ -> Prelude.putStrLn "error"
 
 data Subject =
     Subject {
        name :: String
+       , priority :: Int
+       , complexity :: Int
     } deriving (Show,Generic)
 
-instance FromJSON Subject
 
+instance FromJSON Subject
+instance ToJSON Subject
