02eb92d5436c087d1109c43bb5efd13cc8358efc
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2018-03-03 15:26:10 -0300

    improvements

--- src/web/api/Triggers.hs
+++ src/web/api/Triggers.hs
@@ -12,6 +12,10 @@ import Data.Aeson
 import System.Process.ByteString.Lazy
 import GHC.Generics
 import Data.List
+import Data.Char
+import Data.Time
+import Data.Time.Clock
+import Data.Time.Format
 
 
 main = do
@@ -20,24 +24,46 @@ main = do
         (exitCode, history, stderr) <- readProcessWithExitCode (sourceDirectory ++ "/log.sh") ["--json"] ""
 
         decodedHistory <- (eitherDecode <$> return history) :: IO (Either String [StudyEntry])
-
+        currentTime <- getCurrentTime
 
         case decodedHistory of
             Left msg ->
                 print msg
             Right list ->
-                print  "abc"
 
+                -- print  $  encode <$> list
+                print $ map (\e -> formatTime defaultTimeLocale "%Y-%m-%d" (date e)) list
+                -- print  $ (++) "Done this week: " $ show  $ getDoneThisWeek currentTime list
+                -- print $ formatTime defaultTimeLocale "%V" (date( list !! 0 ))
+
+
+getDoneThisWeek currentDate list =
+    length thisWeekMembers
+    where 
+        thisWeekMembers = filter (\e -> (formatTime defaultTimeLocale "%V" (date e)) == (formatTime defaultTimeLocale "%V" currentDate)) list
+
+
+data Statistics = Statistics {
+    doneThisWeek :: Int
+}
 
 --entities
 -- date, subject
 data StudyEntry  = StudyEntry {
     subject :: String
-    , date :: String
+    , date :: UTCTime
 } deriving (Show,Generic)
 
 
 
 --decoder
-instance FromJSON StudyEntry
+instance FromJSON StudyEntry where
+    parseJSON = withObject "studyentry" $ \o -> do
+        subject <- o .: "subject"
+        dateStr <- o .: "date"
+
+        let date = parseTimeOrError True defaultTimeLocale "%Y-%m-%d %H:%M:%s" dateStr :: UTCTime
+
+        return (StudyEntry subject date)
+
 instance ToJSON StudyEntry
