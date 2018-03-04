aa4f14c5f199017afd0cf2a2ec0950bcdbb82a2a
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2018-03-04 08:43:51 -0300

    top 5 subjects

--- src/web/api/Triggers.hs
+++ src/web/api/Triggers.hs
@@ -16,6 +16,8 @@ import Data.Char
 import Data.Time
 import Data.Time.Clock
 import Data.Time.Format
+import Data.List (sortBy)
+import Data.Function (on)
 
 
 main = do
@@ -28,18 +30,35 @@ main = do
 
         case decodedHistory of
             Left msg ->
-                print msg
+                putStr msg
             Right list ->
-
                 -- print  $  encode <$> list
-                print  $ (++) "Done this week: " $ show  $ getDoneThisWeek currentTime list
+                putStr $ getResultStr list currentTime
                 -- print $ formatTime defaultTimeLocale "%V" (date( list !! 0 ))
 
 
+
+
+getResultStr list currentTime =
+    donesThisWeekCount ++ "\n" ++ top5Str
+
+    where doneThisWeekList = getDoneThisWeek currentTime list
+          donesThisWeekCount = (++) "Done this week: " $ show  $ length $ doneThisWeekList
+          topDone =  sortBy (flip compare `on` snd) $ foldl (\a b -> accOrCreate a b) [] doneThisWeekList
+          top5 = take 5 topDone
+          top5Names = foldl (\a b -> a  ++ (fst b) ++ ", ") "" top5
+          top5Str = "Top five: " ++ top5Names
+
 getDoneThisWeek currentDate list =
-    length thisWeekMembers
-    where 
-        thisWeekMembers = filter (\e -> (formatTime defaultTimeLocale "%V" (date e)) == (formatTime defaultTimeLocale "%V" currentDate)) list
+    filter (\e -> (formatTime defaultTimeLocale "%V" (date e)) == (formatTime defaultTimeLocale "%V" currentDate)) list
+
+
+accOrCreate resultList studyEntry =
+    case filter (\e -> (fst e) == (subject studyEntry)) resultList of
+        (x:_) ->
+           (fst x, 1 + snd x) : delete x resultList
+        _ ->
+            ((subject studyEntry), 1) : resultList
 
 
 data Statistics = Statistics {
