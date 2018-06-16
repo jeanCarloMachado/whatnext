2817e6af3c65612a35e1d5e9d7b48131f3208d4e
jeanCarloMachado <contato@jeancarlomachado.com.br>
2018-06-16 18:55:55 +0200

    improvements

--- deployApi.sh
+++ deployApi.sh
@@ -6,7 +6,5 @@ scp -r api/* blog:"$REMOTE_SERVE_DIR/"
 ssh blog -t '
 pkill -f "webserver" ;
 '
-echo "Deploying"
-ssh -n -f blog "bash -c 'cd $REMOTE_SERVE_DIR/api && nohup python3 webserver.py > /dev/null 2>&1 &'"
+ssh -n -f blog "bash -c 'cd $REMOTE_SERVE_DIR/api && . config.sh && nohup python3 webserver.py > /dev/null 2>&1 &'"
 
-exit 0
