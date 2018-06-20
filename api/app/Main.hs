be282e7f5cae69aa7193dd9f2a2b9f157ae91fd3
jeanCarloMachado <contato@jeancarlomachado.com.br>
2018-06-20 21:52:36 +0200

    refactored

--- api/app/Main.hs
+++ api/app/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE UnicodeSyntax #-}
 {-# LANGUAGE DeriveGeneric #-}
 
 import Web.Scotty
@@ -7,9 +8,10 @@ import GHC.Generics
 import Crypto.Hash
 import           System.Environment
 import           System.Process
-import Data.ByteString.UTF8
+import Data.ByteString.UTF8 as BT
 import Data.Text as Text
 import Data.List (concat)
+import Data.String
 import Web.Cookie
 import Web.Scotty.Cookie
 import Network.Wai.Middleware.Cors
@@ -18,7 +20,77 @@ import Data.Text.Lazy as LT
 import Control.Monad.IO.Class (liftIO)
 
 
-data ApiError = ApiError {status:: Int, message:: String} deriving (Generic, Show)
+main = do
+  wnDir <- getEnv ("WHATNEXT_SRC")
+  scotty 6000 $ do
+    -- middleware myCors
+    middleware myCors
+    get "/scheduler" $ do
+      Web.Scotty.setHeader "Content-Type" "application/json"
+      runAuthenticatedService wnDir $ scheduler wnDir
+
+    post "/login" $ do
+        credentials <- jsonData :: ActionM Credentials
+
+        let loginAsByte = BT.fromString (email credentials)
+            passwordAsByte = BT.fromString (password credentials)
+            ctx = hashInitAlg SHA256
+            ctx1 = hashUpdate ctx loginAsByte
+            ctx2 = hashUpdate ctx1 passwordAsByte
+            end = hashFinalize ctx2
+            result = toString $ digestToHexByteString end
+            cookie = defaultSetCookie { setCookieName = "Authorization", setCookieValue = (BT.fromString result), setCookiePath = (Just (BT.fromString "/")), setCookieDomain = (Just $ BT.fromString ".thewhatnext.net") }
+
+        setCookie cookie
+
+        json (AuthResult (result) )
+
+    get "/detail" $ do
+      text "soon"
+
+    notFound $ do
+      text "Invalid route"
+
+
+scheduler wnDir (Right token) = return $ Right token
+scheduler wnDir (Left token) = do
+  result <- liftIO $ readProcess (wnDir ++ "/" ++ "Scheduler") [] ""
+  return $ Left result
+
+cookieValid wnDir (Right x) = return $ Right x
+cookieValid wnDir (Left x) = do
+  result <- liftIO $ readProcess (wnDir ++ "/" ++ "gateway.sh") ["getEmailByHash", x] ""
+  if Prelude.length result > 3
+  then return $ Left $ result
+  else return $ Right "token invalid"
+  where
+
+cookieExistence cookie =
+      case cookie of
+        Just x -> return $ Left $ T.unpack x
+        Nothing -> return $ Right "Auth cookie needed"
+
+printResult (Right x) = json $ ApiError x
+printResult (Left x) = text $ LT.pack  x
+
+runAuthenticatedService wnDir service =
+      getCookie "Authorization" >>= cookieExistence >>= cookieValid wnDir >>= service >>= printResult
+
+
+myPolicy = CorsResourcePolicy
+    { corsOrigins = (Just (["http://127.0.0.1:3000"], True))
+    , corsMethods = ["GET","PUT","POST","DELETE","OPTIONS"]
+    , corsRequestHeaders = ["Content-Type","Authorization"]
+    , corsExposedHeaders = Nothing
+    , corsMaxAge = Nothing
+    , corsVaryOrigin = True
+    , corsRequireOrigin = False
+    , corsIgnoreFailures = False
+    }
+
+myCors = cors (const $ Just myPolicy)
+
+data ApiError = ApiError {message:: String} deriving (Generic, Show)
 instance ToJSON ApiError
 instance FromJSON ApiError
 
@@ -26,7 +98,7 @@ data SchedulerOk = SchedulerOk {content :: String} deriving (Generic, Show)
 instance ToJSON SchedulerOk
 instance FromJSON SchedulerOk
 
-data Credentials = Credentials { login :: String, password :: String } deriving (Generic, Show)
+data Credentials = Credentials { email :: String, password :: String } deriving (Generic, Show)
 instance ToJSON Credentials
 instance FromJSON Credentials
 
@@ -34,51 +106,3 @@ data AuthResult = AuthResult { authHash :: String } deriving (Generic, Show)
 instance ToJSON AuthResult
 instance FromJSON AuthResult
 
-
-testAuth wnDir token = do
-  result <- readProcess (wnDir ++ "/" ++ "gateway.sh") ["getEmailByHash", tokenStr] ""
-  if Prelude.length result > 3
-  then return (Just result)
-  else return Nothing
-  where
-  tokenStr = T.unpack token
-
-
-procX f wnDir = do
-  token <- liftIO $ f
-  result <- liftIO $ readProcess (wnDir ++ "/" ++ "Scheduler") [] ""
-  case token of
-    Just x -> text $ LT.pack result
-    Nothing -> json (ApiError 9 "token invalid")
-
-
-main = do
-
-  wnDir <- getEnv ("WHATNEXT_SRC")
-  scotty 5000 $ do
-    middleware simpleCors
-
-    get "/scheduler" $ do
-      cookie <- getCookie "Authorization"
-      Web.Scotty.setHeader "Content-Type" "application/json"
-      case cookie of
-        Just x -> procX (testAuth wnDir x) wnDir
-        Nothing -> json $ ApiError 6 "Auth cookie needed"
-
-    post "/login" $ do
-        credentials <- jsonData :: ActionM Credentials
-
-        let loginAsByte = fromString (login credentials)
-            passwordAsByte = fromString (password credentials)
-            ctx = hashInitAlg SHA256
-            ctx1 = hashUpdate ctx loginAsByte
-            ctx2 = hashUpdate ctx1 passwordAsByte
-            end = hashFinalize ctx2
-            result = toString $ digestToHexByteString end
-            cookie = defaultSetCookie { setCookieName = "Authorization", setCookieValue = (fromString result), setCookiePath = (Just (fromString "/")), setCookieDomain = (Just $ fromString ".thewhatnext.net") }
-
-        setCookie cookie
-
-        json (AuthResult (result) )
-    notFound $ do
-      text "Invalid route"
